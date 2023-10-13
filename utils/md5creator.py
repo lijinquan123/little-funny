737d2f0834ad463d5b086aa8f8181e520a483c88
lijinquan123 <3291742120@qq.com>
2023-10-13 17:59:10 +0800

    块支持指定单位

--- utils/md5creator.py
+++ utils/md5creator.py
@@ -9,13 +9,28 @@ import traceback
 from pathlib import Path
 
 
+def scaler(chunk_size: int, dw: str):
+    supports = ['t', 'g', 'm', 'k']
+    chunk_size = int(chunk_size)
+    dw = dw.lower()
+    is_matched = False
+    while supports:
+        d = supports.pop(0)
+        if d == dw:
+            is_matched = True
+        if is_matched:
+            chunk_size *= 1024
+    return chunk_size
+
+
 def md5creator(args):
     # 参数处理
     input_file = Path(args.input)
     output_file = args.output
     if output_file is None:
         output_file = input_file.parent / f'{input_file.name}.jm'
-    chunk_size = args.chunk_size
+    chunk_size = args.chunk_size.strip()
+    chunk_size = scaler(chunk_size[:-1], chunk_size[-1])
     show = args.show
     # 实现
     start = 0
@@ -45,7 +60,8 @@ def main():
     parser = argparse.ArgumentParser(usage='MD5 Creator', description=' --help')
     parser.add_argument(dest='input', type=str, help='input file')
     parser.add_argument('-o', '--output', required=False, type=str, help='output file', dest='output')
-    parser.add_argument('-cs', '--chunk-size', type=int, default=10 * 1024 * 1024, help='chunk size', dest='chunk_size')
+    parser.add_argument('-k', '--chunk-size', type=str, default='10M',
+                        help='chunk size, such as 1k, 1m, 1g, 1t', dest='chunk_size')
     parser.add_argument('-s', '--show', action='store_true', help='show each chunk md5sum', dest='show')
     error_code = 0
     try:
